1ed6e95839f6b92358c05181c1c70acf9ffe4730
cyril <cyril@dkeystone.com>
2012-02-26 23:41:50 +0100

    keeping track of time of post

--- create.php
+++ create.php
@@ -2,14 +2,15 @@
 
 	$room = isset($_GET['room']) ? $_GET['room'] : false;
 	$msg = isset($_GET['msg']) ? $_GET['msg'] : false;
+	$time = time();
 	
 	if(!$room || !$msg) {
-		die(0);
+		die("-1");
 	}
 	
+	$entry = '{"time":"' . $time . '","msg":"' . $msg . '"}';
 	$logfile = "./logs/" . $room . ".txt";
 	$fh = fopen($logfile, 'w') or die("can't open file");
-	$stringData = $msg . "\n\n";
-	fwrite($fh, $stringData);
+	fwrite($fh, $entry);
 	fclose($fh);
 ?>
--- log.php
+++ log.php
@@ -1,13 +1,15 @@
 <?php
 	$msg = isset($_POST['msg']) ? $_POST['msg'] : false;
 	$room = isset($_POST['room']) ? $_POST['room'] : false;
+	$time = time();
 
 	if(!$msg) {
 		die("-1");
 	} else {
+		$entry = ',{"time":' . $time . ',"msg":"' . $msg . '"}';
 		$logfile = "./logs/" . $room . ".txt";
 		$fh = fopen($logfile, 'a');
-		fwrite($fh, $msg."\n");
+		fwrite($fh, $entry);
 		fclose($fh);
 		echo("200");
 	}
